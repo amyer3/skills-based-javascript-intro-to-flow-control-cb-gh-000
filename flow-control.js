9847a948408fdb7bf470d973b07980b8269ae3ff
Alexander Myers <alexander.j.myers@icloud.com>
2019-04-16 17:18:33 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -15,7 +15,13 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-  return "You have an age!"
+  if (age <= 12 ) {
+    return "You are a kid"
+  } else if (age >= 13 && age <= 19) {
+    return "You are a teenager!"
+  } else {
+    return "You are a grownup"
+  }
 
 }
 
